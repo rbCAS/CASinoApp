9a534f07ace13bedb5617a7979aeab0de27bdea7
Raffael Schmid <raf@nine.ch>
2013-02-08 13:35:16 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CASinoApp::Application.config.secret_token = '0c6c0cc68f79adab09ac46e2c463ff2852851f60dec40cbffb813c079ea80a246e5525cad36a11fd5b50c9ef5c14e72f92f2e6c45a0ec4281da16446647351ca'
