763847daee0e42dd3f8a8c7d734aa738866432b6
Raffael Schmid <raf@nine.ch>
2013-03-12 15:12:35 +0100

    raise an error if the cas config file could not be found

--- config/initializers/casino_core.rb
+++ config/initializers/casino_core.rb
@@ -1,1 +1,3 @@
+config_file = File.join(Rails.root, 'config', 'cas.yml')
+raise "Could not find cas config file (#{config_file})" unless File.exists?(config_file)
 CASinoCore.setup Rails.env, application_root: Rails.root
