7ad6b1139cb526ca151d22502e031337ef0dd1b1
Raffael Schmid <raf@nine.ch>
2013-03-12 14:11:28 +0100

    the session is not used

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,18 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-if ENV['SECRET_TOKEN'].present?
-  CASinoApp::Application.config.secret_token = ENV['SECRET_TOKEN']
-elsif ENV['RAILS_GROUPS'] != 'assets'
-  raise <<-ERROR
-
-  You must generate a unique secret token for your CASinoApp.
-
-  If you are deploying via capistrano, please ensure that your `config/deploy.rb` contains
-  the `casinoapp:setup_configs` and `errbit:symlink_configs` tasks from `config/deploy.example.rb`.
-  Next time you deploy, your secret token will be automatically generated.
-
-  If you are deploying to Heroku, please run the following command to set your secret token:
-  heroku config:add SECRET_TOKEN="$(bundle exec rake secret)"
-  ERROR
-end
+CASinoApp::Application.config.secret_token = '82bd3084a8ca1c49d3f4d988e458dfd210716fc957a637722a80d3ca0d1571832a954b7710cbff92dfe293796b9f01af1c8b19568eb7ebeaefade27a4b596257'
