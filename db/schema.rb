cd0c4f1a1985eca8a447ec49f0afff8fe8d0fd36
Nils Caspar <ncaspar@me.com>
2014-08-27 20:45:16 +0200

    Update CASino

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131029185928) do
+ActiveRecord::Schema.define(version: 20140827184143) do
 
   create_table "casino_login_tickets", force: true do |t|
     t.string   "ticket",     null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "casino_login_tickets", ["ticket"], name: "index_casino_login_tickets_on_ticket", unique: true
@@ -27,26 +27,25 @@ ActiveRecord::Schema.define(version: 20131029185928) do
     t.integer  "granter_id",   null: false
     t.string   "pgt_url",      null: false
     t.string   "granter_type", null: false
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "casino_proxy_granting_tickets", ["granter_type", "granter_id"], name: "index_casino_proxy_granting_tickets_on_granter", unique: true
   add_index "casino_proxy_granting_tickets", ["granter_type", "granter_id"], name: "index_proxy_granting_tickets_on_granter", unique: true
   add_index "casino_proxy_granting_tickets", ["iou"], name: "index_casino_proxy_granting_tickets_on_iou", unique: true
   add_index "casino_proxy_granting_tickets", ["ticket"], name: "index_casino_proxy_granting_tickets_on_ticket", unique: true
-  add_index "casino_proxy_granting_tickets", ["ticket"], name: "index_proxy_granting_tickets_on_ticket", unique: true
 
   create_table "casino_proxy_tickets", force: true do |t|
-    t.string   "ticket",                                               null: false
-    t.text     "service",                  limit: 255,                 null: false
-    t.boolean  "consumed",                             default: false, null: false
-    t.integer  "proxy_granting_ticket_id",                             null: false
-    t.datetime "created_at",                                           null: false
-    t.datetime "updated_at",                                           null: false
+    t.string   "ticket",                                   null: false
+    t.text     "service",                                  null: false
+    t.boolean  "consumed",                 default: false, null: false
+    t.integer  "proxy_granting_ticket_id",                 null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "casino_proxy_tickets", ["proxy_granting_ticket_id"], name: "altered_casino_proxy_tickets_on_pgt_id"
+  add_index "casino_proxy_tickets", ["proxy_granting_ticket_id"], name: "casino_proxy_tickets_on_pgt_id"
   add_index "casino_proxy_tickets", ["ticket"], name: "index_casino_proxy_tickets_on_ticket", unique: true
 
   create_table "casino_service_rules", force: true do |t|
@@ -55,33 +54,33 @@ ActiveRecord::Schema.define(version: 20131029185928) do
     t.string   "name",                       null: false
     t.string   "url",                        null: false
     t.boolean  "regex",      default: false, null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "casino_service_rules", ["url"], name: "index_casino_service_rules_on_url", unique: true
 
   create_table "casino_service_tickets", force: true do |t|
-    t.string   "ticket",                                                null: false
-    t.text     "service",                   limit: 255,                 null: false
+    t.string   "ticket",                                    null: false
+    t.text     "service",                                   null: false
     t.integer  "ticket_granting_ticket_id"
-    t.boolean  "consumed",                              default: false, null: false
-    t.boolean  "issued_from_credentials",               default: false, null: false
-    t.datetime "created_at",                                            null: false
-    t.datetime "updated_at",                                            null: false
+    t.boolean  "consumed",                  default: false, null: false
+    t.boolean  "issued_from_credentials",   default: false, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "casino_service_tickets", ["ticket"], name: "index_casino_service_tickets_on_ticket", unique: true
-  add_index "casino_service_tickets", ["ticket_granting_ticket_id"], name: "altered_casino_service_tickets_on_tgt_id"
+  add_index "casino_service_tickets", ["ticket_granting_ticket_id"], name: "casino_service_tickets_on_tgt_id"
 
   create_table "casino_ticket_granting_tickets", force: true do |t|
     t.string   "ticket",                                             null: false
-    t.string   "user_agent"
+    t.text     "user_agent"
     t.integer  "user_id",                                            null: false
     t.boolean  "awaiting_two_factor_authentication", default: false, null: false
     t.boolean  "long_term",                          default: false, null: false
-    t.datetime "created_at",                                         null: false
-    t.datetime "updated_at",                                         null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "casino_ticket_granting_tickets", ["ticket"], name: "index_casino_ticket_granting_tickets_on_ticket", unique: true
@@ -90,8 +89,8 @@ ActiveRecord::Schema.define(version: 20131029185928) do
     t.integer  "user_id",                    null: false
     t.string   "secret",                     null: false
     t.boolean  "active",     default: false, null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "casino_two_factor_authenticators", ["user_id"], name: "index_casino_two_factor_authenticators_on_user_id"
@@ -100,8 +99,8 @@ ActiveRecord::Schema.define(version: 20131029185928) do
     t.string   "authenticator",    null: false
     t.string   "username",         null: false
     t.text     "extra_attributes"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "casino_users", ["authenticator", "username"], name: "index_casino_users_on_authenticator_and_username", unique: true
